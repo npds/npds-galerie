80048ecc1736c095187c0ec831be09cfb01b2886
jeanpierre <jean.barbary@wanadoo.fr>
2020-10-09 08:03:34 +0200

    maj sql for full utf8

--- npds_galerie/install/npds_galerie.sql
+++ npds_galerie/install/npds_galerie.sql
@@ -4,10 +4,10 @@
 CREATE TABLE tdgal_cat (
   id int(11) NOT NULL auto_increment,
   cid int(11) NOT NULL default '0',
-  nom varchar(150) NOT NULL default '',
+  nom varchar(600) COLLATE utf8mb4_unicode_ci NOT NULL default '',
   acces tinyint(4) NOT NULL default '0',
-  PRIMARY KEY  (id)
-) TYPE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Structure de la table `tdgal_com`
@@ -15,12 +15,12 @@ CREATE TABLE tdgal_cat (
 CREATE TABLE tdgal_com (
   id int(11) NOT NULL auto_increment,
   pic_id int(11) NOT NULL default '0',
-  user varchar(60) NOT NULL default '',
-  comment text NOT NULL,
-  comhostname varchar(60) NOT NULL default '',
-  comtimestamp varchar(14) default NULL,
-  PRIMARY KEY  (id)
-) TYPE=MyISAM DEFAULT CHARSET=utf8;
+  user varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  comment text COLLATE utf8mb4_unicode_ci NOT NULL,
+  comhostname varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  comtimestamp varchar(14) COLLATE utf8mb4_unicode_ci default NULL,
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Structure de la table `tdgal_gal`
@@ -28,11 +28,11 @@ CREATE TABLE tdgal_com (
 CREATE TABLE tdgal_gal (
   id int(11) NOT NULL auto_increment,
   cid int(11) NOT NULL default '0',
-  nom varchar(150) NOT NULL default '',
-  date varchar(14) default NULL,
+  nom varchar(600) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  date varchar(14) COLLATE utf8mb4_unicode_ci default NULL,
   acces tinyint(4) NOT NULL default '0',
-  PRIMARY KEY  (id)
-) TYPE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 #
 INSERT INTO tdgal_gal VALUES (1, -1, 'Import', NULL, -127);
 # --------------------------------------------------------
@@ -43,13 +43,13 @@ INSERT INTO tdgal_gal VALUES (1, -1, 'Import', NULL, -127);
 CREATE TABLE tdgal_img (
   id int(11) NOT NULL auto_increment,
   gal_id int(11) NOT NULL default '0',
-  name varchar(40) NOT NULL default '',
-  comment varchar(255) NOT NULL default '',
+  name varchar(160) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  comment varchar(1000) COLLATE utf8mb4_unicode_ci NOT NULL default '',
   view int(11) NOT NULL default '0',
   ordre int(11) NOT NULL default '0',
   noaff int(1) unsigned default '0',
-  PRIMARY KEY  (id)
-) TYPE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 #
 # Structure de la table `tdgal_vot`
@@ -57,9 +57,9 @@ CREATE TABLE tdgal_img (
 CREATE TABLE tdgal_vot (
   id int(11) NOT NULL auto_increment,
   pic_id int(11) NOT NULL default '0',
-  user varchar(60) NOT NULL default '',
+  user varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL default '',
   rating tinyint(4) NOT NULL default '0',
-  ratinghostname varchar(60) NOT NULL default '',
-  ratingtimestamp varchar(14) default NULL,
-  PRIMARY KEY  (id)
-) TYPE=MyISAM DEFAULT CHARSET=utf8;
+  ratinghostname varchar(320) COLLATE utf8mb4_unicode_ci NOT NULL default '',
+  ratingtimestamp varchar(14) COLLATE utf8mb4_unicode_ci default NULL,
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
