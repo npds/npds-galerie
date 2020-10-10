e1abc16a80c07761aaeab27853da7f407a1c0e9c
jeanpierre <jean.barbary@wanadoo.fr>
2020-10-10 10:38:26 +0200

    year version blocs

--- npds_galerie/admin/pages.php
+++ npds_galerie/admin/pages.php
@@ -14,13 +14,13 @@
 /* MAJ conformité XHTML pour REvolution 10.02 par jpb/phr en mars 2010  */
 /* MAJ Dev - 2011                                                       */
 /* MAJ jpb, phr - 2017 renommé npds_galerie pour Rev 16                 */
-/* v 3.0                                                                */
+/* v 3.1                                                                */
 /************************************************************************/
 
 $PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['title']="[french]Galerie d'images[/french][english]Pictures galery[/english][spanish]galeria de imagenes[/spanish]+";
 $PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['js']=array($nuke_url.'/modules/npds_galerie/js/jquery.watermark.min.js');
 $PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['run']="yes";
-$PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['blocs']="2";
+$PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['blocs']="-1";
 $PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['TinyMce']=1;
 $PAGES['modules.php?ModPath='.$ModPath.'&ModStart=gal*']['TinyMce-theme']="short";
 
